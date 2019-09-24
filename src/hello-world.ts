9c958cffb71c82ed5436ac8bcd616d7265c5d986
Jalaj Minda <jaminda@microsoft.com>
2019-09-24 11:28:24 +0530

    minor update

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -5,6 +5,12 @@ import * as io from '@actions/io';
 
 async function run() {
   try {
+    console.log("starting the execution...");
+
+    const bash: string = await io.which('bash', true);
+    console.log(bash);
+    await exec.exec(`"${bash}"`, ['az --version']);
+
     const nameToGreet = core.getInput('who-to-greet');
     if (nameToGreet == 'Octocat') {
         // the Octocat doesn't want to be greeted here!
@@ -14,11 +20,7 @@ async function run() {
         const time = (new Date()).toTimeString();
         core.setOutput("time", time);
     }
-    console.log("starting the execution...");
-
-    const bash: string = await io.which('bash', true);
-
-    await exec.exec(`"${bash}"`, ['az --version']);
+    
 
     // Get the JSON webhook payload for the event that triggered the workflow
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
