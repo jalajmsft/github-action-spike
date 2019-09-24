9b4496dcdf75a569bfd879f19b07bf1c56a8e3ec
Jalaj Minda <jaminda@microsoft.com>
2019-09-24 12:27:05 +0530

    update

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -2,11 +2,11 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as io from '@actions/io';
-
+const { execSync } = require('child_process');
 async function run() {
   try {
     console.log("starting the execution...");
-
+    let stdout = execSync('ls');
     const bash: string = await io.which('bash', true);
     console.log(bash);
     await exec.exec(`"${bash}"`, ['sample.sh']);
