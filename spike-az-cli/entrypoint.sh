0f69d5d6a24d9b79657cd09e2821e1a547b45881
Jalaj Minda <jaminda@microsoft.com>
2019-10-21 20:19:23 +0530

    added auth to az cli

--- spike-az-cli/entrypoint.sh
+++ spike-az-cli/entrypoint.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
-echo "command is $1"
+echo "command is $sampleInput"
 
-sh -c "$1"
+echo "login into az"
+servicePrincipalId = credsObject["clientId"];
+servicePrincipalKey = credsObject["clientSecret"];
+tenantId = credsObject["tenantId"];
+subscriptionId = credsObject["subscriptionId"];
+
+if [[ -n "$servicePrincipalId" ]] && [[ -n "$servicePrincipalKey" ]] && [[ -n "$tenantId" ]]
+then
+  az login --service-principal --username "$servicePrincipalId" --password "$servicePrincipalKey" --tenant "$tenantId"
+else
+  echo "One of the required parameters for Azure Login is not set: servicePrincipalId, servicePrincipalKey, tenantId." >&2
+  exit 1
+fi
+
+if [[ -n "$subscriptionId" ]]
+then
+  az account set --s "$subscriptionId"
+else
+  SUBSCRIPTIONS=$(az account list)
+  if [[ ${#SUBSCRIPTIONS[@]} -gt 1 ]]
+  then
+    echo "subscriptionId is not set." >&2
+    exit 1
+  fi
+fi
+
+sh -c "$sampleInput"
