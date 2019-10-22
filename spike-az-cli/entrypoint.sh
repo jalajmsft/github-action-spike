4b602c3b4bae4d84892cb3928561b6d216754cfe
Jalaj Minda <jaminda@microsoft.com>
2019-10-22 12:42:10 +0530

    minor fix

--- spike-az-cli/entrypoint.sh
+++ spike-az-cli/entrypoint.sh
@@ -14,13 +14,20 @@ tenantId = $4
 # subscriptionId = $2[subscriptionId]
 subscriptionId = "abc"
 
-if [[ -n "$servicePrincipalId" ]] && [[ -n "$servicePrincipalKey" ]] && [[ -n "$tenantId" ]]
+if [[ -n "$2" ]] && [[ -n "$3" ]] && [[ -n "$4" ]]
 then
-  az login --service-principal --username "$servicePrincipalId" --password "$servicePrincipalKey" --tenant "$tenantId"
+  az login --service-principal --username "$2" --password "$3" --tenant "$4"
 else
   echo "One of the required parameters for Azure Login is not set: servicePrincipalId, servicePrincipalKey, tenantId." >&2
   exit 1
 fi
+# if [[ -n "$servicePrincipalId" ]] && [[ -n "$servicePrincipalKey" ]] && [[ -n "$tenantId" ]]
+# then
+#   az login --service-principal --username "$servicePrincipalId" --password "$servicePrincipalKey" --tenant "$tenantId"
+# else
+#   echo "One of the required parameters for Azure Login is not set: servicePrincipalId, servicePrincipalKey, tenantId." >&2
+#   exit 1
+# fi
 
 # if [[ -n "$subscriptionId" ]]
 # then
