91bfe14ce7d32be311970b154acef429dcaefc93
Jalaj Minda <jaminda@microsoft.com>
2019-10-22 12:35:53 +0530

    updated entrypoint.sh

--- spike-az-cli/entrypoint.sh
+++ spike-az-cli/entrypoint.sh
@@ -2,16 +2,17 @@
 
 echo "command is $1"
 
-echo  "dummy obj is $3"
+echo  "dummy obj is $5"
 
-echo "val 1 is $3['abc']"
-echo "val 1 is $3[abc]"
+echo "val 1 is $5['abc']"
+echo "val 1 is $5[abc]"
 
 echo "login into az"
-servicePrincipalId = $2[clientId]
-servicePrincipalKey = $2[clientSecret]
-tenantId = $2[tenantId]
-subscriptionId = $2[subscriptionId]
+servicePrincipalId = $2
+servicePrincipalKey = $3
+tenantId = $4
+# subscriptionId = $2[subscriptionId]
+subscriptionId = "abc"
 
 if [[ -n "$servicePrincipalId" ]] && [[ -n "$servicePrincipalKey" ]] && [[ -n "$tenantId" ]]
 then
@@ -21,16 +22,16 @@ else
   exit 1
 fi
 
-if [[ -n "$subscriptionId" ]]
-then
-  az account set --s "$subscriptionId"
-else
-  SUBSCRIPTIONS=$(az account list)
-  if [[ ${#SUBSCRIPTIONS[@]} -gt 1 ]]
-  then
-    echo "subscriptionId is not set." >&2
-    exit 1
-  fi
-fi
+# if [[ -n "$subscriptionId" ]]
+# then
+#   az account set --s "$subscriptionId"
+# else
+#   SUBSCRIPTIONS=$(az account list)
+#   if [[ ${#SUBSCRIPTIONS[@]} -gt 1 ]]
+#   then
+#     echo "subscriptionId is not set." >&2
+#     exit 1
+#   fi
+# fi
 
 sh -c "$1"
