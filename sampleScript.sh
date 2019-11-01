988ffcc587d0653022c755a19afc078b2e6a9605
Jalaj Minda <jaminda@microsoft.com>
2019-11-01 15:39:33 +0530

    secret 2

--- sampleScript.sh
+++ sampleScript.sh
@@ -2,5 +2,5 @@ echo "what is your name?"
 remark="az --version"
 echo "I am $remark too!!"
 $remark
-az vm create -n jaminda-azcli-t3 -g jaminda-az-cli --image debian --subscription RMDev --authentication-type all --admin-password ${{ secrets.Azure_cred_login_obj }} --admin-username jaminda
+az vm create -n jaminda-azcli-t4 -g jaminda-az-cli --image debian --subscription RMDev --authentication-type all --admin-password ${{ secrets.Azure_cred_login_obj }} --admin-username jaminda
 az account show
